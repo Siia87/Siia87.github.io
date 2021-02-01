af536813fd369514f4e9726e5e822979a58abe0e
Sofia Premuzaj <sofia.premuzaj@gmail.com>
2021-02-01 11:27:06 +0100

    Add first version

---
+++ precache-manifest.2233680e11376aa243ed8538f60d9816.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b7e9d24e347bc2f05c",
+    "url": "/css/app.105fe421.css"
+  },
+  {
+    "revision": "25474c14a260e63bd0093387805ac9dc",
+    "url": "/img/BeerLogo1.25474c14.png"
+  },
+  {
+    "revision": "123d5c89f2bd6ea251cca8f0bb069f38",
+    "url": "/img/beerBottles.123d5c89.png"
+  },
+  {
+    "revision": "89f60d89f2b815a3989a84f1dd2e93be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b7e9d24e347bc2f05c",
+    "url": "/js/app.547d6316.js"
+  },
+  {
+    "revision": "687cc2a5b504e590532a",
+    "url": "/js/chunk-vendors.d418f799.js"
+  },
+  {
+    "revision": "cacc6148e1cd0eaf252dbec3fbf4c48d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
